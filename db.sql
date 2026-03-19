183629decb7397ee9c281effa4e8110b85974371
MAML8 <marcanantins@gmail.com>
2026-03-19 12:49:41 -0300

    git inicializado

---
+++ db.sql
@@ -0,0 +1,22 @@
+CREATE TABLE User(
+    VARCHAR(50) user_name PRIMARY KEY,
+    VARCHAR(100) display_name NOT NULL,
+    TINYTEXT password_ NOT NULL,
+    SMALLINT clearance_level NOT NULL Default 0
+);
+
+CREATE TABLE Study(
+    VARCHAR(50) access_name PRIMARY KEY,
+    TINYTEXT display_name NOT NULL,
+    SMALLINT clearance_level_to_acces NOT NULL Default 1
+);
+
+CREATE TABLE Log(
+    TIMESTAMP hora,
+    VARCHAR(50) access_name,
+    PRIMARY KEY(hora, access_name),
+    FOREIGN KEY(access_name) REFERENCES Study(access_name),
+    VARCHAR(50) user_name NOT NULL,
+    FOREIGN KEY(user_name) REFERENCES User(user_name),
+    TEXT texto NOT NULL
+);
